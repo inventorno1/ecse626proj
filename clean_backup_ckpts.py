7a56cb35cfd4b117df3fd117756a86a9c5926f42
Ethan Honey <my.ethanhoney@gmail.com>
2024-11-27 11:28:11 -0500

    Script for removing some backup ckpts

---
+++ clean_backup_ckpts.py
@@ -0,0 +1,21 @@
+import os
+
+if __name__ == '__main__':
+
+    backup_interval = 5
+    last_backup_ckpt = 145
+    new_backup_interval = 50
+    out_dir = "/cim/ehoney/ecse626proj/experiment1"
+    backup_ckpts_dir = os.path.join(out_dir, 'backup_ckpts')
+
+    for epoch in range(backup_interval, last_backup_ckpt+backup_interval, backup_interval):
+        if epoch % 50 != 0:
+            ckpt_path = os.path.join(backup_ckpts_dir, f'epoch{epoch}.pth.tar')
+            try:
+                os.remove(ckpt_path)
+                print(f"File '{ckpt_path}' has been deleted successfully.")
+            except Exception as e:
+                print(f"Error occurred while deleting the file: {e}")
+
+    # for ckpt in sorted(os.listdir(backup_ckpts_dir)):
+    #     print(ckpt)
